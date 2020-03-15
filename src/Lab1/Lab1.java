482222f86549c098fca6b5e9598cba64eed243d5
IhorSherstiuk <30436504+Nortus222@users.noreply.github.com>
2020-03-15 09:40:37 +0200

    Fix Lab1.java

--- src/Lab1/Lab1.java
+++ src/Lab1/Lab1.java
@@ -1,4 +1,32 @@
 package Lab1;
 
 public class Lab1 {
+    public static void main(String[] args){
+        double S = 0;
+        int a = 1;
+        int b = 2;
+        int n = 5;
+        int m = 5;
+        final int c = 0;
+
+        if(a <= c && c <= n || b <= 0 && 0 <= m){
+            System.out.println("Division by zero");
+
+        }else{
+
+            for(float i = (float)a; i<=n; i++){
+                for(float j = (float)b; j<=m; j++){
+                    S+=((double)(i/j)/i);
+                }
+
+            }
+            System.out.println(S);
+        }
+
+
+
+
+
+
+    }
 }
