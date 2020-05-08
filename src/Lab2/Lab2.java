efdd8d886adfab653930163ca102d80e8449a296
IhorSherstiuk <30436504+Nortus222@users.noreply.github.com>
2020-05-08 15:54:11 +0300

    Fix Lab2.java

--- src/Lab2/Lab2.java
+++ src/Lab2/Lab2.java
@@ -4,18 +4,13 @@ package Lab2;
 public class Lab2 {
     public static void main(String[] args) {
 
-        Matrix test = new Matrix(3, 5);
-
-        test.create();
-
-        test.output();
+        Matrix test = new Matrix(5, 3);
 
         test.transplonate();
 
-        test.sumMAx();
+        test.sumMax();
 
     }
-
 
 
 }
--- src/Lab2/Matrix.java
+++ src/Lab2/Matrix.java
@@ -4,82 +4,95 @@ import java.util.Random;
 
 
 public class Matrix {
-    int a;
-    int b;
-    int [][] B;
+   private int a;
+   private int b;
+   private int[][] B;
+   private int[][] B1;
 
-    public Matrix(int a, int b){
+    public Matrix(int a, int b) {
         this.a = a;
         this.b = b;
         this.B = new int[this.a][this.b];
+        this.create();
+        this.output(this.B, this.a, this.b);
     }
-    public void create(){
 
+
+    public void create() {
         Random tmp = new Random();
-        for(int i = 0; i < a; i++ ){
-            for(int j = 0; j < b; j++){
-                B[i][j] = (char)tmp.nextInt(100);
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                B[i][j] = (char) tmp.nextInt(100);
             }
         }
 
     }
 
 
-    public void output(){
-        for(int i = 0; i < a; i++ ){
-            for(int j = 0; j< b; j++){
-                System.out.print(B[i][j] + " ");
+    public void output(int[][] m, int a, int b) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.printf("%3d", m[i][j]);
             }
             System.out.println();
         }
     }
 
 
-
-    public void transplonate(){
-        if(a == b){
+    public void transplonate() {
+        if (a == b) {
             System.out.println();
             System.out.println("Транспонована матриця: ");
             System.out.println();
-            for(int i = 0; i < a; i++ ){
-                for(int j = 0; j< b; j++){
-                    System.out.print(B[j][i] + " ");
+            for (int i = 0; i < a; i++) {
+                for (int j = i + 1; j < b; j++) {
+                    int tmp = B[i][j] ;
+                    B[i][j] = B[j][i];
+                    B[j][i] = tmp;
                 }
-                System.out.println();
             }
-        }else{
-            int [][] B1 = new int[b][a];
+            System.out.println();
+            this.output(B, a, b);
+        } else {
+            B1 = new int[b][a];
 
-            for(int i = 0; i < b; i++ ){
-                for(int j = 0; j < a; j++){
+            for (int i = 0; i < b; i++) {
+                for (int j = 0; j < a; j++) {
                     B1[i][j] = B[j][i];
                 }
             }
             System.out.println();
             System.out.println("Транспонована матриця: ");
             System.out.println();
-
-            for(int i = 0; i < b; i++ ){
-                for(int j = 0; j< a; j++){
-                    System.out.print(B1[i][j] + " ");
-                }
-                System.out.println();
-            }
+            this.output(B1, b, a);
         }
     }
 
-    public void sumMAx(){
-        int max ;
-        int sum = 0;
 
-        for(int i = 0; i < b; i++){
-            max = 0;
-            for(int j = 0; j < a; j++){
-                if(B[j][i] > max){
-                    max = B[j][i];
+    public void sumMax() {
+        int max;
+        int sum = 0;
+        if(a==b) {
+            for (int i = 0; i < b; i++) {
+                max = B[0][i];
+                for (int j = 1; j < a; j++) {
+                    if (B[j][i] > max) {
+                        max = B[j][i];
+                    }
                 }
+                sum += max;
             }
-            sum += max;
+        }else{
+            for (int i = 0; i < a; i++) {
+                max = B1[0][i];
+                for (int j = 1; j < b; j++) {
+                    if (B1[j][i] > max) {
+                        max = B1[j][i];
+                    }
+                }
+                sum += max;
+            }
+
         }
         System.out.println();
         System.out.println("Сума найбільших елементів кожного стовпця матриці = " + sum);
