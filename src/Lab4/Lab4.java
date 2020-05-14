177a47775f68d8403807c69fe3aadd8d57d8c7fb
IhorSherstiuk <30436504+Nortus222@users.noreply.github.com>
2020-05-14 12:22:23 +0300

    Adding Lab4.java and Student.java

---
+++ src/Lab4/Lab4.java
@@ -0,0 +1,77 @@
+/*
+
+Lab4.java
+
+Main class
+
+Created by Ihor Sherstiuk on 05/14/20.
+
+Copyright © 2020 Ihor Sherstiuk. All rights reserved.
+
+ */
+
+package Lab4;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ * Class that creates a list of Students and performs operations
+ */
+public class Lab4 {
+    public static void main(String[] args) {
+
+        String[] names = {"Ivan", "Alex", "Petro", "Dima", "Denis"};
+
+        Student[] students = new Student[5];
+        Random tmp = new Random();
+
+
+        for (int i = 0; i < students.length; i++) {
+            students[i] = new Student(names[i],
+                    tmp.nextInt((27-17)+1)+17,
+                    tmp.nextInt((6-1)+1)+1,
+                    tmp.nextInt((126-121)+1)+121,
+                    1f+tmp.nextFloat()*(100f-1f),
+                    "KPI");
+            students[i].getInfo();
+        }
+
+        Arrays.sort(students, new SortByAge());
+        System.out.println("-------Sorted-------------------By Age---------(Up)");
+        for (int i = 0; i < students.length; i++) {
+            students[i].getInfo();
+        }
+
+        Arrays.sort(students, new SortByPerformance());
+        System.out.println("-------Sorted-------------------By Performance---------(Down)");
+        for (int i = 0; i < students.length; i++) {
+            students[i].getInfo();
+        }
+    }
+}
+
+/**
+ * Class for comparing age of two students
+ */
+class SortByAge implements Comparator<Student> {
+    @Override
+    public int compare(Student a, Student b){
+        return a.age - b.age;
+    }
+}
+
+/**
+ * Class for comparing a performance of two students
+ */
+class SortByPerformance implements Comparator<Student>{
+    @Override
+    public int compare(Student a, Student b){
+        if (b.performance < a.performance)
+            return -1;
+        if (b.performance > a.performance)
+            return 1;
+        return 0;
+    }
+}
---
+++ src/Lab4/Student.java
@@ -0,0 +1,66 @@
+/*
+
+Student.java
+
+Class with student methods.
+
+Created by Ihor Sherstiuk on 05/14/20.
+
+Copyright © 2020 Ihor Sherstiuk. All rights reserved.
+
+ */
+
+package Lab4;
+
+import java.util.Comparator;
+
+/**
+ * Class that stores all student information and methods
+ */
+public class Student {
+    public float performance; /*Student's level of performance (0-100%)*/
+    public int age; /* Student's age */
+    private String name; /* Student's name */
+    private int course; /* Student's course (1-6)*/
+    private int spec; /* Student's speciality code*/
+    private String univ; /*Name of Student's University*/
+
+    public Student(String name, int age, int course, int speciality, float performance, String university){
+        /* Sets initial Student's data */
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        this.spec = speciality;
+        this.performance = performance;
+        this.univ = university;
+    }
+
+    public void transferUniv(String newUniversity){
+        /* Transfers Student to a new University */
+        this.univ = newUniversity;
+        System.out.println("Update completed");
+    }
+
+    public void transferSpec(int newSpeciality){
+        /* Transfers Student to a new speciality */
+        this.spec = newSpeciality;
+        System.out.println("Update completed");
+    }
+
+    public void expulsion(){
+        /* Expels Student from university */
+        this.spec = 0;
+        this.course = 0;
+        this.performance = 0f;
+        this.univ = "None";
+        System.out.println("Update completed");
+    }
+
+    public void getInfo() {
+        /* Prints Student's information */
+        System.out.printf("Name: %s \nAge: %d \nCourse: %d \nSpeciality: %d \nPerformance: %.2f \nUniversity: %s \n\n",
+                name, age, course, spec, performance, univ);
+    }
+
+}
+
